ee226eb2ad35197b3fd190880e89d56aa320d2f6
Victoria <MVictoriaV>
2018-04-03 22:37:27 -0300

    It was removed method @Before

--- ejercicio2/src/test/java/ar/edu/untref/aydoo/DrinkVendingMachineTest.java
+++ ejercicio2/src/test/java/ar/edu/untref/aydoo/DrinkVendingMachineTest.java
@@ -1,55 +1,52 @@
 package ar.edu.untref.aydoo;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class DrinkVendingMachineTest {
 
-    private DrinkVendingMachine drinkVendingMachine;
-    private Glass aGlass;
-
-    @Before
-    public void glassAndMachineAreInstantiated() {
-        this.drinkVendingMachine = new DrinkVendingMachine();
-        this.aGlass = new Glass();
-    }
-
     @Test
     public void shouldMakeCoffeeWithMilkWith10Sugar() {
+
         int amountOfSugar = 10;
+        Glass aGlass = new Glass();
+        DrinkVendingMachine drinkVendingMachine = new DrinkVendingMachine();
 
-        this.drinkVendingMachine.doCoffeeWithMilkWithNSugar(aGlass, amountOfSugar);
+        drinkVendingMachine.doCoffeeWithMilkWithNSugar(aGlass, amountOfSugar);
 
-        Assert.assertEquals(amountOfSugar, this.aGlass.getAmountOfSugar());
-        Assert.assertTrue(this.aGlass.hasCoffee());
-        Assert.assertTrue(this.aGlass.hasSugar());
-        Assert.assertFalse(this.aGlass.hasTea());
+        Assert.assertEquals(amountOfSugar, aGlass.getAmountOfSugar());
+        Assert.assertTrue(aGlass.hasCoffee());
+        Assert.assertTrue(aGlass.hasSugar());
+        Assert.assertFalse(aGlass.hasTea());
     }
 
     @Test
     public void shouldMakeTeaWithMilkWith0Sugar() {
 
         int amountOfSugar = 0;
+        Glass aGlass = new Glass();
+        DrinkVendingMachine drinkVendingMachine = new DrinkVendingMachine();
 
-        this.drinkVendingMachine.doTeaWithMilkWithNSugar(this.aGlass, amountOfSugar);
+        drinkVendingMachine.doTeaWithMilkWithNSugar(aGlass, amountOfSugar);
 
         Assert.assertEquals(amountOfSugar, aGlass.getAmountOfSugar());
-        Assert.assertFalse(this.aGlass.hasCoffee());
-        Assert.assertFalse(this.aGlass.hasSugar());
-        Assert.assertTrue(this.aGlass.hasTea());
+        Assert.assertFalse(aGlass.hasCoffee());
+        Assert.assertFalse(aGlass.hasSugar());
+        Assert.assertTrue(aGlass.hasTea());
     }
 
     @Test
     public void shouldMakeTeaWithMilkWith3Sugar() {
 
         int amountOfSugar = 3;
+        Glass aGlass = new Glass();
+        DrinkVendingMachine drinkVendingMachine = new DrinkVendingMachine();
 
-        this.drinkVendingMachine.doTeaWithMilkWithNSugar(this.aGlass, amountOfSugar);
+        drinkVendingMachine.doTeaWithMilkWithNSugar(aGlass, amountOfSugar);
 
-        Assert.assertEquals(amountOfSugar, this.aGlass.getAmountOfSugar());
-        Assert.assertFalse(this.aGlass.hasCoffee());
-        Assert.assertTrue(this.aGlass.hasSugar());
-        Assert.assertTrue(this.aGlass.hasTea());
+        Assert.assertEquals(amountOfSugar, aGlass.getAmountOfSugar());
+        Assert.assertFalse(aGlass.hasCoffee());
+        Assert.assertTrue(aGlass.hasSugar());
+        Assert.assertTrue(aGlass.hasTea());
     }
 }

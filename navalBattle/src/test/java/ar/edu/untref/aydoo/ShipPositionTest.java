2c3cd3cd196d74693c8cf0d799d94fae699a4431
Victoria <MVictoriaV>
2018-04-21 17:19:09 -0300

    add test receive a shot

--- navalBattle/src/test/java/ar/edu/untref/aydoo/ShipPositionTest.java
+++ navalBattle/src/test/java/ar/edu/untref/aydoo/ShipPositionTest.java
@@ -15,6 +15,16 @@ public class ShipPositionTest {
         Assert.assertFalse(result);
     }
 
+    @Test
+    public void aShipPositionReceiveAShot() {
+
+        ShipPosition aShipPosition = this.createAShipPosition(2, 4);
+
+        aShipPosition.receiveAShot();
+
+        Assert.assertTrue(aShipPosition.hasShot());
+    }
+
     private ShipPosition createAShipPosition(Integer horizontalPosition, Integer verticalPosition){
         Position aPosition = new Position(horizontalPosition, verticalPosition);
         return new ShipPosition(aPosition);

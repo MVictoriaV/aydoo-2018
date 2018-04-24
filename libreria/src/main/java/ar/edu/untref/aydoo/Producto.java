edda26c0bede5355801d0512abba709e880f4859
Victoria <MVictoriaV>
2018-04-23 21:25:45 -0300

    Agregamos el metodo constructor de Productos con parametros

--- libreria/src/main/java/ar/edu/untref/aydoo/Producto.java
+++ libreria/src/main/java/ar/edu/untref/aydoo/Producto.java
@@ -2,7 +2,18 @@ package ar.edu.untref.aydoo;
 
 public class Producto {
 
+    private Double precio;
+    private String descripcion;
+    private String codigo;
+
+    public Producto(Double precio, String descripcion, String codigo) {
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+
+    }
+
     public Double getPrecioDeVenta() {
-        return 10d;
+        return precio;
     }
 }
--- libreria/src/test/java/ar/edu/untref/aydoo/ProductoTest.java
+++ libreria/src/test/java/ar/edu/untref/aydoo/ProductoTest.java
@@ -8,10 +8,20 @@ public class ProductoTest {
     @Test
     public void obtenerPrecioDeVentaDeUnProductoRetornaValor10() {
 
-        Producto lapiz = new Producto();
+        Producto lapiz = new Producto(10d, "Lapiz", "A1");
 
         Double precio = lapiz.getPrecioDeVenta();
 
         Assert.assertEquals(new Double(10), precio);
+    }
+
+    @Test
+    public void obtenerPrecioDeVentaDeUnProductoRetornaValor9Con99() {
+
+        Producto lapizColorNegro = new Producto(9.99, "Lapiz color negro","A2");
+
+        Double precio = lapizColorNegro.getPrecioDeVenta();
+
+        Assert.assertEquals(new Double(9.99), precio);
     }
 }

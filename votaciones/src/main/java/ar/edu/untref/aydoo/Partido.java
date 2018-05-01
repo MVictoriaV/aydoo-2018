3be3e3d694677fae8b9ec8880c1c0c3fba3423b2
Victoria <MVictoriaV>
2018-05-01 00:22:27 -0300

    Implementacion metodo cargar candidato y contiene candidato

--- votaciones/src/main/java/ar/edu/untref/aydoo/Partido.java
+++ votaciones/src/main/java/ar/edu/untref/aydoo/Partido.java
@@ -1,11 +1,33 @@
 package ar.edu.untref.aydoo;
 
+import java.util.*;
+
 public class Partido {
 
-    public void cargarCandidatoAlListado(Candidato unCandidato) {
+    private String nombre;
+    private Map<Provincia, List<Candidato>> listadoCandidato;
+
+    public Partido(String nombre) {
+        this.nombre = nombre;
+        listadoCandidato = new HashMap<>();
     }
 
-    public boolean tieneCandidatos() {
-        return true;
+    public void cargarCandidatoAlListado(Candidato unCandidato, Provincia provincia) {
+        if (listadoCandidato.containsKey(provincia)) {
+            if (!contieneCandidato(unCandidato, provincia)){
+                listadoCandidato.get(provincia).add(unCandidato);
+                listadoCandidato.put(provincia, listadoCandidato.get(provincia));
+            }
+        } else {
+            List<Candidato> candidatos = new ArrayList<>();
+            candidatos.add(unCandidato);
+            listadoCandidato.put(provincia, candidatos);
+        }
+    }
+
+    public boolean contieneCandidato(Candidato unCandidato, Provincia provincia) {
+        Optional<Candidato> anOptional = listadoCandidato.get(provincia).stream().filter(candidato ->
+                (candidato.equals(unCandidato))).findFirst();
+        return anOptional.isPresent();
     }
 }
--- votaciones/src/test/java/ar/edu/untref/aydoo/PartidoTest.java
+++ votaciones/src/test/java/ar/edu/untref/aydoo/PartidoTest.java
@@ -8,11 +8,24 @@ public class PartidoTest {
     @Test
     public void cargarCandidatoAlListadoDeberiaTenerUnCandidato(){
 
-        Partido partidoX = new Partido();
+        Partido partidoX = new Partido("XYZ");
         Candidato cosme = new Candidato("Cosme", "Fulanito", 12345678);
 
-        partidoX.cargarCandidatoAlListado(cosme);
+        partidoX.cargarCandidatoAlListado(cosme, Provincia.BUENOS_AIRES);
 
-        Assert.assertTrue(partidoX.tieneCandidatos());
+        Assert.assertTrue(partidoX.contieneCandidato(cosme, Provincia.BUENOS_AIRES));
+    }
+
+    @Test
+    public void cargar2CandidatosAlListado(){
+
+        Partido partidoX = new Partido("XYZ");
+        Candidato cosme = new Candidato("Cosme", "Fulanito", 12345678);
+        Candidato pepe = new Candidato("Pepe", "Fulanito", 12345679);
+
+        partidoX.cargarCandidatoAlListado(cosme, Provincia.BUENOS_AIRES);
+        partidoX.cargarCandidatoAlListado(pepe, Provincia.BUENOS_AIRES);
+
+        Assert.assertTrue(partidoX.contieneCandidato(cosme, Provincia.BUENOS_AIRES));
     }
 }

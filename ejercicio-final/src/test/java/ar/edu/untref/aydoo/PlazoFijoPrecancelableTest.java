5f58ce324b0dc97101a155230bc759f62b1eefe9
Victoria <MVictoriaV>
2018-07-16 22:30:50 -0300

    Se prueba excepcion cuando monto es 0

--- ejercicio-final/src/test/java/ar/edu/untref/aydoo/PlazoFijoPrecancelableTest.java
+++ ejercicio-final/src/test/java/ar/edu/untref/aydoo/PlazoFijoPrecancelableTest.java
@@ -19,4 +19,29 @@ public class PlazoFijoPrecancelableTest {
 
         Assert.assertEquals(gananciaEsperada, gananciaReal, 0.1);
     }
+
+    @Test
+    public void seCalculaUnaGananciaConUnInteresDel10PorcDebeRetornar3698() throws CampoIncorrectoExcepcion {
+
+        Double monto = 200000d;
+        Integer interes = 15;
+        Integer plazoAcordado = 90;
+        Integer plazoReal = 30;
+        Inversion pfp = new PlazoFijoPrecancelable(monto, interes, plazoAcordado, plazoReal);
+
+        Double gananciaReal = pfp.calcularGanancia();
+        Double gananciaEsperada = 3698.6d;
+
+        Assert.assertEquals(gananciaEsperada, gananciaReal, 0.1);
+    }
+
+    @Test(expected = CampoIncorrectoExcepcion.class)
+    public void lanzaExcepcionCuandoIngresaMontoConValor0() throws CampoIncorrectoExcepcion{
+
+        Double monto = 0d;
+        Integer interes = 10;
+        Integer plazoAcordado = 365;
+        Integer plazoReal = 365;
+        new PlazoFijoPrecancelable(monto, interes, plazoAcordado, plazoReal);
+    }
 }
